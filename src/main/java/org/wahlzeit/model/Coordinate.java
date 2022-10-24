a93f190b6a19804f2576833129a2495ccb6a0b9b
FendVario1 <andi.hellmich@gmx.de>
2022-10-24 22:09:39 +0200

    implement isEqual, equals, hashCode and tests

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -30,4 +30,26 @@ public class Coordinate {
     public double getZ() {
         return z;
     }
+
+    public double getDistance(Coordinate coordinate) {
+        return Math.sqrt(Math.pow(coordinate.getX() - x, 2) + Math.pow(coordinate.getY() - y, 2) + Math.pow(coordinate.getZ() - z, 2));
+    }
+
+    public boolean isEqual(Coordinate coordinate) {
+        return Math.abs(coordinate.getX() - x) <= delta && Math.abs(coordinate.getY() - y) <= delta && Math.abs(coordinate.getZ() - z) <= delta;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != Coordinate.class)
+            return false;
+        if (this == obj)
+            return true;
+        return isEqual((Coordinate) obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
 }
